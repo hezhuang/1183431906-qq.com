491c1f633524c0f477a2beaaa5118754c614b071
hezhuang <hezhuang@hezhuangdeMac.AirDream>
2015-07-22 11:09:05 +0800

    first

---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by hezhuang on 15/7/22.
+//  Copyright (c) 2015年 hezhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by hezhuang on 15/7/22.
+//  Copyright (c) 2015年 hezhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"--------------------");
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
